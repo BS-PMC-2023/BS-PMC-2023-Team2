8550fc99b1ae70adeffb0d614fc018e80f1d9c79
ZingerMarat <93285999+ZingerMarat@users.noreply.github.com>
2023-04-05 10:34:32 +0300

    confirm order controller

--- server/src/controllers/orderController.ts
+++ server/src/controllers/orderController.ts
@@ -1,6 +1,7 @@
 import { Request, Response, NextFunction } from "express";
 import { Types } from "mongoose";
 import Order from "../models/Order";
+import Reservation from "../models/Reservation";
 
 const makeOrder = async (req: Request, res: Response) => {
   const { userId, itemName, DateFrom, DateTo } = req.body;
@@ -25,7 +26,22 @@ const makeOrder = async (req: Request, res: Response) => {
 const confirmOrder = async (req: Request, res: Response) => {
   const { studentId, itemId, DateFrom, DateTo } = req.body;
 
+  try {
+    const newReservation = new Reservation({
+      student: new Types.ObjectId(studentId),
+      item: new Types.ObjectId(itemId),
+      DateFrom: new Date(DateFrom),
+      DateTo: new Date(DateTo),
+      status: false,
+    });
+
+    await newReservation.save();
+
+    res.sendStatus(200);
+  } catch (err) {
+    res.sendStatus(404);
+  }
 };
 
 
-export { makeOrder };
+export { makeOrder, confirmOrder };
