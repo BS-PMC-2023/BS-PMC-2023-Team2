b4a9f8d0021eb30c807c132899fbb4559b4587a5
ZingerMarat <93285999+ZingerMarat@users.noreply.github.com>
2023-04-05 10:42:15 +0300

    router added

--- server/src/routers/orderRouter.ts
+++ server/src/routers/orderRouter.ts
@@ -1,9 +1,12 @@
 import { Router } from 'express';
-import {makeOrder} from '../controllers/orderController'
+import {makeOrder, confirmOrder} from '../controllers/orderController'
 import verifyToken from '../middleware/verifyToken'
+
 
 const router = Router();
 
 router.post('/makeOrder', verifyToken, makeOrder)
+router.post('/confirmOrder', verifyToken, confirmOrder)
+
 
 export default router;
