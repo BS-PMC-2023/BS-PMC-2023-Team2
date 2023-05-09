c46a41251b82fa6e23ce36c8d307f71932ce23c2
hay12el <hay12el@gmail.com>
2023-05-09 14:53:15 +0300

    add pathes to main router

--- storage-app/src/router/mainRouter.tsx
+++ storage-app/src/router/mainRouter.tsx
@@ -1,15 +1,21 @@
 import React, { FC } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Home from '../pages/Home/Home';
-import LogIn from '../pages/LogIn/LogIn';
-import MainPage from '../pages/MainPage/MainPage';
-import Student from '../pages/Student/Student';
-import Teacher from '../pages/Teacher/Teacher';
-import Admin from '../pages/Admin/Admin';
-import Exl from '../components/Exl/Exl';
-import AddOrder from '../components/AddOrder/AddOrder';
-import AddOrderGroup from '../components/AddOrderGroup/AddOrderGroup';
+import Home from "../pages/Home/Home";
+import LogIn from "../pages/LogIn/LogIn";
+import MainPage from "../pages/MainPage/MainPage";
+import Student from "../pages/Student/Student";
+import Teacher from "../pages/Teacher/Teacher";
+import Admin from "../pages/Admin/Admin";
+import AdminLayout from "../pages/AdminLayout/AdminLayout";
+import AddOrder from "../components/AddOrder/AddOrder";
+import AddOrderGroup from "../components/AddOrderGroup/AddOrderGroup";
 import Error from "../pages/404/Error";
+import AddProduct from "../components/AddProduct/AddProduct";
+import Exl from "../components/Exl/Exl";
+import FoultyItems from "../components/FoultyItems/FoultyItems";
+import Summery from "../components/Summery/Summery";
+import ManageOrders from "../components/ManageOrders/ManageOrders";
+// import StudentOrders from "../components/StudentsOrders/StudentOrders";
 
 const MainRouter: FC = ({}) => {
   return (
@@ -18,13 +24,22 @@ const MainRouter: FC = ({}) => {
         <Route path="/" element={<MainPage />}>
           <Route index element={<Home />} />
           <Route path="/loginsignup/:is" element={<LogIn />} />
-          <Route path="/Student" element={<Student />} />
-          <Route path="/Student/AddOrder" element={<AddOrder />} />
-          <Route path="/Student/AddOrderGroup" element={<AddOrderGroup />} />
+          <Route path="Student" element={<Home />}>
+            {/* <Route
+              path="studentGetOrders"
+              element={<StudentOrders />}
+            /> */}
+            <Route path="AddOrder" element={<AddOrder />} />
+            <Route path="AddOrderGroup" element={<AddOrderGroup />} />
+          </Route>
           <Route path="/Teacher" element={<Teacher />} />
-          <Route path="Admin" element={<Admin />}>
+          <Route path="Admin" element={<AdminLayout />}>
             <Route index element={<Admin />} />
-            <Route path="Exl" element={<Admin />} />
+            <Route path="AddProducts" element={<AddProduct />} />
+            <Route path="Exl" element={<Exl />} />
+            <Route path="FaultyPtod" element={<FoultyItems />} />
+            <Route path="summery" element={<Summery />} />
+            <Route path="manageorders" element={<ManageOrders />} />
           </Route>
           <Route path="*" element={<Error />} />
         </Route>
