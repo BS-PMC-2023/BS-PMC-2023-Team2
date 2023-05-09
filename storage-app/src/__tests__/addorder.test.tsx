53ce3e684ac4a9158d5059f509feefce86a62f17
yatirlugassi <yatirlugassi@gmail.com>
2023-05-09 19:26:48 +0300

    fixed addorder unittest

--- storage-app/src/__tests__/addorder.test.tsx
+++ storage-app/src/__tests__/addorder.test.tsx
@@ -6,41 +6,44 @@ import { Provider } from "react-redux";
 import { store } from "../redux/Store";
 import axios from 'axios';
 import AddOrder from '../components/AddOrder/AddOrder';
+import AddOrderGroup from '../components/AddOrderGroup/AddOrderGroup';
 jest.mock('axios');
 
 
 describe('AddOrder', () => {
-    beforeEach(() => {
-      (axios.post as jest.Mock).mockResolvedValue({
-        data: {
-          token: 'testToken',
-          user: {
-            userName: 'testUserName',
-            isAdmin: 'true'
-          }
+  beforeEach(() => {
+    (axios.post as jest.Mock).mockResolvedValue({
+      data: {
+        token: 'testToken',
+        user: {
+          userName: 'testUserName',
+          isAdmin: 'true'
         }
-      });
+      }
+    });
 
 render(
-    <Provider store={store}>
-      <BrowserRouter>
-        <AddOrder/>
-      </BrowserRouter>
-    </Provider>
-  );
-});
+      <Provider store={store}>
+        <BrowserRouter>
+          <AddOrderGroup/>
+        </BrowserRouter>
+      </Provider >
+    );
+  });
 
 
   it('renders the Show Inventory button', () => {
-    expect(screen.getByRole('button', { name: "Check Avilability" })).toBeInTheDocument();
+    expect(screen.getByRole('button', { name: "Order" })).toBeInTheDocument();
   });
 
   it('renders the header', () => {
     expect(screen.getByRole('heading',{level:2})).toBeInTheDocument();
   });
+
   it('testing label from',()=>{ 
     expect(screen.getByLabelText(/From Date/)).toBeInTheDocument();
   });
+
   it('testing label to',()=>{ 
     expect(screen.getByLabelText(/To Date/)).toBeInTheDocument();
   });
