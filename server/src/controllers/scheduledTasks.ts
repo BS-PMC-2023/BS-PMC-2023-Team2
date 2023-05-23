9fbc17a5d71a60af816753d727498537dc303809
NoamTrabelsi <93206473+NoamTrabelsi@users.noreply.github.com>
2023-05-23 19:42:10 +0300

    server for Notification System Theacher

---
+++ server/src/controllers/scheduledTasks.ts
@@ -0,0 +1,5 @@
+import cron from 'node-cron';
+import Reservation from '../models/Reservation'
+import Student from '../models/Student'
+import Item from '../models/Item'
+import nodemailer from "nodemailer";
