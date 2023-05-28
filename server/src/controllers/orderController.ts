4237b392eb1f6fb68f83cfa7f23e76e29732b809
yatirlugassi <yatirlugassi@gmail.com>
2023-05-28 19:28:57 +0300

    Added Function and Export

--- server/src/controllers/orderController.ts
+++ server/src/controllers/orderController.ts
@@ -4,6 +4,7 @@ import { IOrder, IStudent } from "../interfaces/interfaces";
 import Order from "../models/Order";
 import Reservation from "../models/Reservation";
 import Student from "../models/Student";
+import Item from "../models/Item";
 
 const studentGetOrders = async (req: Request, res: Response) => {
   try {
@@ -12,10 +13,25 @@ const studentGetOrders = async (req: Request, res: Response) => {
 
     const orders = await Order.find({ userId: userId });
     const reservation = await Reservation.find({ student: userId });
-
-    console.log(orders);
-    console.log(reservation);
-    res.sendStatus(200);
+    const itemsIds = reservation.map((reserv) => reserv.item);
+    const items = await Item.find({ _id: { $in: itemsIds } });
+    //@ts-ignore
+    const resWithItems = [];
+    reservation.forEach((rsv) => {
+      const itm = items.find((x) => x._id.toString() == rsv.item.toString());
+      resWithItems.push({
+        rsv: rsv,
+        itm: itm,
+      });
+    });
+    res
+      .send({
+        orders: orders,
+        reservation: reservation,
+        //@ts-ignore
+        resWithItems: resWithItems,
+      })
+      .status(200);
   } catch (error: any) {
     console.log(error.message);
     res.send(404);
@@ -38,9 +54,10 @@ const AdminGetOrders = async (req: Request, res: Response) => {
       const theUser: IStudent | undefined = users.find(
         (us) => us._id.toString() == orders[i].userId.toString()
       );
+
       let a: str = {
         //@ts-ignore
-        _id: orders[i]._Id,
+        _id: orders[i]._id,
         itemName: orders[i].itemName,
         DateFrom: orders[i].DateFrom,
         DateTo: orders[i].DateTo,
@@ -60,7 +77,6 @@ const makeOrder = async (req: Request, res: Response) => {
   const { itemName, DateFrom, DateTo } = req.body.obj;
 
   try {
-    
     const newOrder = new Order({
       userId: new Types.ObjectId(req.body.userId),
       itemName: itemName,
@@ -79,18 +95,48 @@ const makeOrder = async (req: Request, res: Response) => {
 };
 
 const confirmOrder = async (req: Request, res: Response) => {
-  const { studentId, itemId, DateFrom, DateTo } = req.body;
+  const { userId, itemName, DateFrom, DateTo } = req.body.item;
 
   try {
-    const newReservation = new Reservation({
-      student: new Types.ObjectId(studentId),
-      item: new Types.ObjectId(itemId),
-      DateFrom: new Date(DateFrom),
-      DateTo: new Date(DateTo),
-      status: false,
-    });
+    console.log(req.body);
 
-    await newReservation.save();
+    // find the available items.
+    const reservations = await Reservation.find({
+      $or: [
+        { DateFrom: { $gte: DateFrom }, DateTo: { $lte: DateTo } },
+        { DateFrom: { $lte: DateFrom }, DateTo: { $gte: DateTo } },
+        { DateFrom: { $gte: DateFrom, $lte: DateTo } },
+        { DateTo: { $gte: DateFrom, $lte: DateTo } },
+      ],
+    });
+    const itemsIds = reservations.map((x) => x.item);
+    const items = await Item.find({
+      kind: itemName,
+      _id: { $nin: itemsIds },
+      condition: true,
+    }).select("-subItems");
+
+    // if there are an items available at the requested period
+    // take the first one available and make new Reservation related to the student.
+    if (items.length != 0) {
+      const newReservation = new Reservation({
+        student: new Types.ObjectId(userId),
+        item: new Types.ObjectId(items[0]._id),
+        DateFrom: new Date(DateFrom),
+        DateTo: new Date(DateTo),
+        status: true,
+      });
+      // save the new reservation in DB.
+      await newReservation.save();
+
+      // console.log(req.body.item._id);
+
+      // delete the order
+      await Order.findOneAndDelete({ _id: req.body.item._id });
+    } else {
+      // no available items
+      throw "Pay Attention! there are no available items in this category!";
+    }
 
     res.sendStatus(200);
   } catch (err) {
@@ -118,4 +164,23 @@ const enterToWaitingList = async (req: Request, res: Response) => {
   }
 };
 
-export { makeOrder, confirmOrder, enterToWaitingList, AdminGetOrders, studentGetOrders };
+const StudentReturnItem = async (req: Request, res: Response) => {
+  const { itm, rsv } = req.body.itemToReturn;
+  
+  try {
+    await Reservation.findByIdAndDelete({ _id: rsv._id });
+    res.sendStatus(200);
+  } catch (error: any) {
+    console.log(error.message);
+    res.sendStatus(404);
+  }
+};
+
+export {
+  makeOrder,
+  confirmOrder,
+  enterToWaitingList,
+  AdminGetOrders,
+  studentGetOrders,
+  StudentReturnItem,
+};
